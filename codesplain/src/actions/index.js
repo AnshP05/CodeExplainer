97e394fc8c1b2a36467192b56d0d13c04b2bb4cb
AwesomeAnsh69 <23parasharansh@gmail.com>
2025-11-24 19:39:25 -0800

    Further setting up the react frontend creating the major components

---
+++ codesplain/src/actions/index.js
@@ -0,0 +1,31 @@
+"use server"
+
+export async function explain(prevState, formData) {
+    const code = formData.get("code");
+    const language = formData.get("language");
+
+    try {
+        await fetch(`${import.meta.env.VITE_API_URL}/explain-code`, {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({code, language}),
+        });
+
+        if(!res.ok) {
+            return {
+                success: false,
+                error: "Failed to fetch explanation. Please try again.",
+            }
+        }
+        const data = await res.json();
+        return {
+            success: true,
+            data
+        }
+    } catch (error) {
+        return {
+            success: false,
+            error: `An Error Occurred: ${error?.message}`,
+        }
+    }
+}
